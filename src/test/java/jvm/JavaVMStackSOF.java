d7dda3773cb3f0afd3ab8ccbf28641d36cd17bd0
GitHubFeiLong <1696741038@qq.com>
2020-04-08 21:30:35 +0800

    JVM第二章看完

---
+++ src/test/java/jvm/JavaVMStackSOF.java
@@ -0,0 +1,23 @@
+package jvm;
+
+/**
+ * -Xss1m
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String ... args){
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try{
+            oom.stackLeak();
+        } catch (Throwable throwable){
+            System.err.println("stack length:" + oom.stackLength);
+            throw throwable;
+        }
+    }
+}
